8288e504953b35f8eeefb9353a718ce4317bb99e
naram Kim <sylvester127@naver.com>
2020-01-25 01:02:27 +0900

    feat: Add Member mapper and Commented out without error

--- src/main/java/com/stockmarket/www/dao/MemberDao.java
+++ src/main/java/com/stockmarket/www/dao/MemberDao.java
@@ -3,6 +3,7 @@ package com.stockmarket.www.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.stockmarket.www.entity.Member;
 
@@ -14,15 +15,14 @@ public interface MemberDao {
 	Member getMember(int id);
 	Member getMemberByEmail(String query);	
 	int getMemberRank(int id);
-	int updateMember(int id, long vmoney);
-	int updateMember(int id, String cardPos);
+	int updateMember(Member member);
+//	int updateMember(@Param("id") int id, @Param("cardPos") String cardPos);
 	int insertMember(Member member);
 	
 	//프로필 이미지 교체 (imgChange는 더미)
-	int updateMember(int writerId, int profileImg, String imgChange);
-	int updateMember(int id, String newPwd, String pwdChange);
+//	int updateMember(int writerId, int profileImg, String imgChange);
+//	int updateMember(int id, String newPwd, String pwdChange);
 	
 	//닉네임 중복확인
 	Boolean isDuplicatedId(String nickname);
-	
 }
