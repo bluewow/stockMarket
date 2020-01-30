e2d582fe59f5abcf59288a27fc0cc1f5361d7477
bluewow <dogseen@gmail.com>
2020-01-30 17:20:39 +0900

    RecordAssert @Mapper 추가

--- src/main/java/com/stockmarket/www/dao/RecordAssetDao.java
+++ src/main/java/com/stockmarket/www/dao/RecordAssetDao.java
@@ -2,8 +2,11 @@ package com.stockmarket.www.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.stockmarket.www.entity.RecordAsset;
 
+@Mapper
 public interface RecordAssetDao {
 	
 	// 자산추이 그래프
