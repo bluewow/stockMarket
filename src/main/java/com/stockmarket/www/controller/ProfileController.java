07bb492c644f5eded44cc3c7eb19bf53a9feb778
seolkd <seolkd.e@gmail.com>
2020-01-31 18:06:48 +0900

    닉네임 패스워드 글자 길이 제한 / 안쓰는 임포트 삭제

--- src/main/java/com/stockmarket/www/controller/ProfileController.java
+++ src/main/java/com/stockmarket/www/controller/ProfileController.java
@@ -2,7 +2,6 @@ package com.stockmarket.www.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
