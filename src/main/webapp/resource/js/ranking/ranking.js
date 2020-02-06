444eaad7b6d8fb107b74eaf48b77e5d3bce3f701
naram Kim <sylvester127@naver.com>
2020-02-06 15:59:13 +0900

    fix: Fix profile image changing

--- src/main/webapp/resource/js/ranking/ranking.js
+++ src/main/webapp/resource/js/ranking/ranking.js
@@ -1,8 +1,7 @@
 window.addEventListener("message", function (e) {
 	if (e.data[0] == "selectPhoto"){
-		console.log("selectPhoto : " + e.data[1]);
 		let profile = document.querySelector("#myRank .profileImg img");
-		profile.src = "/images/profile/"+ e.data[1] +".png";
+		profile.src = "/resource/images/profile/"+ e.data[1] +".png";
 		
 		let rank = profile.parentNode.previousElementSibling.innerText;
 		
@@ -13,7 +12,7 @@ window.addEventListener("message", function (e) {
 		
 		for(let i = 0; i < profile2.length; i++){
 			if(id == profile2[i].innerText)
-				profile2[i].previousElementSibling.firstChild.src = "/images/profile/"+ e.data[1] +".png";			
+				profile2[i].previousElementSibling.firstChild.src = "/resource/images/profile/"+ e.data[1] +".png";			
 		}
 	}
 });
