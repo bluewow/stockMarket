93627e023dc99d22e7a6ff66c64fe5141c7c7196
bluewow <dogseen@gmail.com>
2020-02-09 12:51:49 +0900

    분석 일부 string 변경

--- src/main/webapp/resource/js/trade/analysis.js
+++ src/main/webapp/resource/js/trade/analysis.js
@@ -57,14 +57,14 @@ function textToggle() {
 	
 	button[1].onclick = function() {
 		if(button[1].value == "변동성 중간") 
-			button[1].value = "±5%~±15%";
+			button[1].value = "±5% ~ 15%";
 		else 
 			button[1].value = "변동성 중간";
 	};
 	
 	button[2].onclick = function() {
 		if(button[2].value == "변동성 낮음") 
-			button[2].value = "±0% ~ ±5%";
+			button[2].value = "±0% ~ 5%";
 		else 
 			button[2].value = "변동성 낮음";
 	};
